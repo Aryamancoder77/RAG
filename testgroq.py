08ddc3b552ce082cd28c09dad2a165d3ddf46a4c
Aryamancoder77 <aryamanbaliyan@gmail.com>
2026-05-08 21:46:35 +0530

    Update testgroq.py

--- testgroq.py
+++ testgroq.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from groq import Groq, GroqError
 
-# Load environment variables from .env file
+# load enviornment 
 load_dotenv()
 
 # Get the API key from environment variable
